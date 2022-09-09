61f642dd3c2603b0e7db40b0f6dac1edabb26a5a
Robby Smith <smith010123@gmail.com>
2022-09-09 14:14:28 -0400

    go

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x51&$qx=a(1_%x$fe59)*+elcmnxkc2-g6^r$(@^8dmvn0i+68'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
