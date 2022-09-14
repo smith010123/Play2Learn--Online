e31d1c01aec659276f610067d2c803762f38053f
Robby Smith <smith010123@gmail.com>
2022-09-14 17:11:35 -0400

    push

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -199,7 +199,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 # Default primary key field type
