12d1957242d59fe63efb2fe327579ab9c59f3545
Robby Smith <smith010123@gmail.com>
2022-09-14 13:24:48 -0400

    css

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -190,8 +190,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
