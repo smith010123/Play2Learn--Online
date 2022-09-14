0f0f609c1bcac033fabc493592bd998ee769ec2c
Robby Smith <smith010123@gmail.com>
2022-09-14 13:29:15 -0400

    django

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+import django_heroku
 import environ
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
@@ -198,6 +199,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
 
 #  BOTTOM OF settings.py
 if os.environ.get('ENVIRONMENT') != 'production':
