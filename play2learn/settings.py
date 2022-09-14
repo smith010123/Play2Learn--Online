ddc08dcfcbe2745c8c762ea2e75b74abbfd6f0fd
Robby Smith <smith010123@gmail.com>
2022-09-14 12:48:41 -0400

    go

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -184,9 +184,9 @@ USE_TZ = True
 # Local Static File Settings
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 #STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / 'static',
+#]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
