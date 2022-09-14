2d3193fce8a1bd162bca57c194ac3406d4aa6132
Robby Smith <smith010123@gmail.com>
2022-09-14 13:43:41 -0400

    test

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -195,7 +195,7 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 #test
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
