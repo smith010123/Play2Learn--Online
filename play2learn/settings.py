70f923f1c2942d3fe718f712338d22aa4557c376
Robby Smith <smith010123@gmail.com>
2022-09-18 11:03:22 -0400

    DEPLOY

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -195,10 +195,12 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 #STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 #test
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ( None)
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
