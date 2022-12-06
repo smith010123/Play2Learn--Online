92205e4bf8ffa1188316c2c018b285c6fbdbe7c9
Robby Smith <smith010123@gmail.com>
2022-12-06 14:34:51 -0500

    Go

--- static/javascript/ah_final.js
+++ static/javascript/ah_final.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function() {
+/*window.addEventListener('load', function() {
    addScore = sessionStorage.getItem("addScore");
    addScoreM = sessionStorage.getItem("addScoreM");
    console.log(addScore);
@@ -10,6 +10,32 @@ window.addEventListener('load', function() {
          score.innerText = addScore;
 
    }
+   if (isNaN(addScoreM)) {
+      scoreM.innerText = 0;
+
+   } else {
+      let scoreM = document.getElementById('scoreM');
+      scoreM.innerText = addScoreM;
+   }
+
+});*/
+
+window.addEventListener('load', function() {
+   addScore = sessionStorage.getItem("addScore");
+   addScoreM = sessionStorage.getItem("addScoreM");
+   console.log(addScore);
+   if (isNaN(addScore) && isNaN(addScoreM)) {
+      score.innerText = 0;
+
+   } else if(isNaN(addScore) && addScoreM > 0){
+      let score = document.getElementById('score');
+         score.innerText = addScoreM;
+
+   } else {
+      let score = document.getElementById('score');
+         score.innerText = addScore;
+   }
+
    if (isNaN(addScoreM)) {
       scoreM.innerText = 0;
 
